597bab1d0f694f006f2ecd8137ec395ec4e5ef73
VAISHALI BUCCHA <HP@YASH.pc.YASH.com>
2025-03-10 18:13:23 +0530

    Fixed game routes and templates

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")  # Your game menu
+    return render_template("index.html")  # Homepage
 
 @app.route('/blackjack')
 def blackjack():
-    return render_template("blackjack.html")  # Your blackjack game
+    return render_template("blackjack.html")  # Blackjack game
 
 @app.route('/xando')
 def tic_tac_toe():
-    return render_template("xando.html")  # Your Tic-Tac-Toe game
+    return render_template("xando.html")  # Tic-Tac-Toe
+@app.route('/guessnumber')
+
+def guess_number():
+    return render_template("guessnumber.html")  # Guess the Number
+
+@app.route('/hangman')
+def hangman():
+    return render_template("hangman.html")  # Hangman game
 
 if __name__ == "__main__":
     app.run(debug=True)
